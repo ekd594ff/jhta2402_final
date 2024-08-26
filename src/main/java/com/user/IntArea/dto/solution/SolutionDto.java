a57c5ceed2992d9334d1ee985bce5b691a746db7
SangBin0902 <SangBin0902@gmail.com>
2024-08-26 11:22:57 +0900

    SolutionDto

---
+++ src/main/java/com/user/IntArea/dto/solution/SolutionDto.java
@@ -0,0 +1,15 @@
+package com.user.IntArea.dto.solution;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class SolutionDto {
+
+    private final String title;
+    private final String description;
+    private final String price;
+}
