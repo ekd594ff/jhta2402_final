dca6539a7b38320751ee5c9182280326faee2260
SangBin0902 <SangBin0902@gmail.com>
2024-08-26 12:04:26 +0900

    solution

--- src/main/java/com/user/IntArea/dto/solution/SolutionDto.java
+++ src/main/java/com/user/IntArea/dto/solution/SolutionDto.java
@@ -1,15 +1,17 @@
 package com.user.IntArea.dto.solution;
 
+import com.user.IntArea.entity.Solution;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-@Builder
+@Data
 public class SolutionDto {
 
     private final String title;
     private final String description;
+    private final String portfolio;
     private final String price;
+
 }
--- src/main/java/com/user/IntArea/service/SolutionService.java
+++ src/main/java/com/user/IntArea/service/SolutionService.java
@@ -1,5 +1,8 @@
 package com.user.IntArea.service;
 
+import com.user.IntArea.dto.solution.SolutionDto;
+import com.user.IntArea.entity.Portfolio;
+import com.user.IntArea.entity.Solution;
 import com.user.IntArea.repository.SolutionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,5 +13,33 @@ public class SolutionService {
 
     private final SolutionRepository solutionRepository;
 
+    // Create
+    public void create(SolutionDto solutionDto) {
 
+        Solution solution = Solution.builder()
+                .title(solutionDto.getTitle())
+                .description(solutionDto.getDescription())
+                .portfolio(null)
+                .price(Integer.parseInt(solutionDto.getPrice()))
+                .build();
+
+        solutionRepository.save(solution);
+    }
+
+    // Read
+    public void read() {
+
+
+    }
+
+    // Update
+    public void update() {
+
+
+    }
+
+    // Delete
+    public void delete() {
+
+    }
 }
