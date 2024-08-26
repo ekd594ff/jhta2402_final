42a3dfa890b44c247bcb315844967852e4ec9783
ekd594ff <ekd594ff@gmail.com>
2024-08-26 16:11:52 +0900

    feature: add scss modules and some components

--- frontend/src/pages/login/signup.jsx
+++ frontend/src/pages/login/signup.jsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import {useNavigate, Link} from "react-router-dom";
 
 function Signup() {
 
