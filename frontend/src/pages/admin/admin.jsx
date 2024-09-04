7d429c92af5cce6717b937e69e01fa21e2738d1b
jin0815b <wlsgus25843@gmail.com>
2024-09-04 12:37:11 +0900

    feat : member filtering sorting 기능

--- frontend/src/pages/admin/admin.jsx
+++ frontend/src/pages/admin/admin.jsx
@@ -1,6 +1,6 @@
 import {Link, Route, Routes, useLocation, useNavigate} from "react-router-dom";
 
-import AdminIndex from "./pages/index.jsx";
+// import AdminIndex from "./pages/index.jsx";
 
 import style from "../../styles/admin-index.module.scss"
 import * as React from 'react';
@@ -11,15 +11,24 @@ import ListItemText from '@mui/material/ListItemText';
 import {useEffect, useLayoutEffect, useState} from "react";
 import axios from "axios";
 import Paper from '@mui/material/Paper';
-import {Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow} from "@mui/material";
-import Stack from "@mui/material/Stack";
-import {Pagination} from "swiper/modules";
+import {
+    DataGrid,
+    GridToolbarContainer,
+    GridToolbarFilterButton,
+    GridToolbarExport,
+    getGridStringOperators
+} from "@mui/x-data-grid";
+// import { GridFilterOperator } from '@mui/x-data-grid'
+import {TablePagination} from "@mui/material";
+import {debounce} from "lodash";
+import {any} from "prop-types";
+
 
 function NestedList() {
     const [open, setOpen] = React.useState(true);
-    const navigate = useNavigate();
-    const handleClick = (location) => {
-        navigate(location);
+
+    const handleClick = () => {
+        setOpen(!open);
     };
 
     return (
@@ -33,89 +42,203 @@ function NestedList() {
                 </ListSubheader>
             }
         >
-            <ListItemButton onClick={() => handleClick("member")}>
-                Member
+            <ListItemButton>
+                <Link to={"member"}><ListItemText primary="Member"/></Link>
             </ListItemButton>
-            <ListItemButton onClick={() => handleClick("company")}>
-                Company
+            <ListItemButton>
+                <Link to={"company"}><ListItemText primary="Company"/></Link>
             </ListItemButton>
-            <ListItemButton onClick={() => handleClick("portfolio")}>
-                Portfolio
+            <ListItemButton>
+                <Link to={"portfolio"}><ListItemText primary="Portfolio"/></Link>
             </ListItemButton>
-            <ListItemButton onClick={() => handleClick("review")}>
-                Review
+            <ListItemButton>
+                <Link to={"review"}><ListItemText primary="Review"/></Link>
             </ListItemButton>
         </List>
     );
 }
+// const columns = [
+//     { field: 'id', headerName: 'ID'},
+//     { field: 'firstName', headerName: 'First name' },
+//     { field: 'lastName', headerName: 'Last name'},
+//     {
+//         field: 'age',
+//         headerName: 'Age',
+//         type: 'number',
+//         width: 90,
+//     },
+//     {
+//         field: 'fullName',
+//         headerName: 'Full name',
+//         description: 'This column has a value getter and is not sortable.',
+//         sortable: false,
+//         valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+//     },
+// ];
+
+// const rows = [
+//     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+//     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+//     { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+//     { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+//     { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+//     { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+//     { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+//     { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+//     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+// ];
+
+function CustomToolbar(props) {
+    return (
+        <GridToolbarContainer>
+            <GridToolbarFilterButton />
+            <GridToolbarExport />
+        </GridToolbarContainer>
+    );
+}
+
+const filterOperators = getGridStringOperators()
+    .filter(
+        (operator) =>
+            operator.value === "contains"
+    )
+    .map((operator) => {
+        return {
+            ...operator
+        };
+    });
 
 function DataTable() {
     const [data, setData] = useState([]);
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(5);
     const [totalCount, setTotalCount] = useState(0);
     const path = useLocation()
     const pathname = path.pathname.split("/admin/")[1];
-    const fetchData = async (pathname,page, size) => {
+    const [columns, setColumns] = useState([]);
+    const [filterModel, setFilterModel] = useState({field: "", value:"" });
+    const [paginationModel, setPaginationModel] = useState({page: 0, pageSize: 5});
+    const [sortModel, setSortModel] = useState({field: "", sort: ""})
+    const fetchData = async (pathname, paginationModel) => {
         try {
+            const {page, pageSize} = paginationModel;
             const response =
                 await axios.get(`/api/${pathname}/admin/list`, {
-                    params: {page, size}
+                    params: {page, pageSize}
                 });
+
+            // 동적으로 컬럼 정의 생성
+            if (response.data.content.length > 0) {
+                const cols = Object.keys(response.data.content[0]).map(key => ({
+                    field: key,
+                    headerName: key.charAt(0).toUpperCase() + key.slice(1), // 첫 글자 대문자
+                    width: 200, // 기본 너비 설정
+                    filterOperators
+                }));
+                setColumns(cols);
+            }
             setData(response.data.content); // 실제 데이터 구조에 맞게 수정
             setTotalCount(response.data.page.totalElements); // 전체 데이터 수
         } catch (error) {
             console.error('데이터를 가져오는 데 오류가 발생했습니다:', error);
+        } finally {
+            // console.log(data);
+        }
+    };
+    const fetchFilterdData = async (filterModel,sortModel,pathname,paginationModel) => {
+        try {
+            const {page, pageSize} = paginationModel;
+            const param = {};
+            param.page = page;
+            param.pageSize = pageSize;
+            if (sortModel) {
+                if (sortModel.field) {
+                    param.sortField = sortModel.field;
+                }
+                if (sortModel.sort) {
+                    console.log("sortValue", sortModel.sort);
+                    param.sort = sortModel.sort;
+                }
+            }
+            if (filterModel.value) {
+                param.filterValue = filterModel.value;
+            }
+            if (filterModel.field) {
+                param.filterColumn = filterModel.field;
+            }
+            const response =
+                await axios.get(`/api/${pathname}/admin/list/filter/contains`, {
+                    params: param    //sortModel, filterModel
+                });
+            // 동적으로 컬럼 정의 생성
+            if (response.data.content.length > 0) {
+                const cols = Object.keys(response.data.content[0]).map(key => ({
+                    field: key,
+                    headerName: key.charAt(0).toUpperCase() + key.slice(1), // 첫 글자 대문자
+                    width: 200, // 기본 너비 설정
+                    filterOperators
+                }));
+                setColumns(cols);
+            }
+            setData(response.data.content); // 실제 데이터 구조에 맞게 수정
+            setTotalCount(response.data.page.totalElements); // 전체 데이터 수
+        } catch (error) {
+            console.error('데이터를 가져오는 데 오류가 발생했습니다:', error);
+        }finally {
+            // console.log(data);
         }
     };
 
     useEffect(() => {
-        fetchData(pathname,page, rowsPerPage); // 페이지는 0부터 시작하므로 +1
-        console.log("page=" + page);
-        console.log("rowsPerPage=" + rowsPerPage);
-        console.log(pathname);
-        console.log(data);
-    }, [pathname,page, rowsPerPage]);
+        // console.log("filterModel.value.length=", filterModel.value.length);
+        // console.log("sortModel.field.length=", sortModel.field.length);
+        let sortField;
+        if (sortModel) {
+            sortField = sortModel.field;
+        }
+        if (filterModel.value || sortField) { // filterModel, sortModel 값 확인 후 분기
+            fetchFilterdData(filterModel, sortModel, pathname, paginationModel); // 매개변수 수정
+        } else {
+            fetchData(pathname, paginationModel);
+        }
 
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
+    }, [paginationModel, sortModel, totalCount, filterModel]);
+
+    useEffect(() => {
+        setPaginationModel({page: 0, pageSize: 5})
+    }, [pathname, sortModel, filterModel]);
+
+    const handleFilterModelChange = (model) => {
+        console.log('filterModel', model);
+        setFilterModel(model.items[0])
     };
-
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0); // 페이지를 0으로 리셋
+    const handleSortModelChange = (model) => {
+        console.log("sortModel[0]",model[0]);
+        console.log("sortModel",model);
+        setSortModel(model[0]);
+    };
+    const handlePaginationModelChange = (model) => {
+        // console.log("paginationModel", model);
+        setPaginationModel(model);
     };
 
     return (
-        <Paper>
-            <TableContainer>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            {data.length ? Object.keys(data[0]).map((column) => (
-                                <TableCell key={column}>{column}</TableCell>
-                            )) : <></>}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {data.map((item) => (
-                            <TableRow key={item.id}>
-                                {Object.keys(item).map((column, index) => (
-                                    <TableCell key={`${item.id}_${index}`}>{`${item[column]}`}</TableCell>
-                                ))}
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[5, 10, 25]}
-                component="div"
-                count={totalCount}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
+        <Paper sx={{height: 400, width: '100%'}}>
+            <DataGrid
+                rows={data}
+                columns={columns}
+                checkboxSelection
+                components={{
+                    Toolbar: CustomToolbar
+                }}
+                pagination
+                filterMode="server" // 클라이언트 측 필터링 또는 서버 측 필터링 설정 (server / client)
+                paginationMode="server"
+                sortingMode="server"
+                // filterModel={filterModel}
+                rowCount={totalCount}
+                onFilterModelChange={handleFilterModelChange}
+                onSortModelChange={handleSortModelChange}
+                onPaginationModelChange={handlePaginationModelChange}
+                paginationModel={paginationModel}
             />
         </Paper>
     );
