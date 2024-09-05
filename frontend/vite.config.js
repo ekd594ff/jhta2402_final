4929f9422f2a5b1c31964d4ae1e3003c06e35648
mahau.master <otter.oh@gmail.com>
2024-09-05 17:29:35 +0900

    fix: setup deploy for dockers

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
-import {defineConfig, loadEnv} from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default ({mode}) => {
+export default ({ mode }) => {
     const env = loadEnv(mode, "../");
     return defineConfig({
         plugins: [react()],
@@ -10,6 +10,7 @@ export default ({mode}) => {
             outDir: "../src/main/resources/static"
         },
         server: {
+            host: '0.0.0.0',
             port: parseInt(env.VITE_REACT_DEV_PORT) || 8082, // .env 설정 없으면 vite 서버 8082 포트에서 실행
             proxy: {
                 '/api': {
