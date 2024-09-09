9493e170ec554d475057b4010a9e8f7ff8f65ca8
typhoon0678 <typhoon0678@gmail.com>
2024-09-09 18:49:20 +0900

    feat: data.sql 작성 중

--- src/main/java/com/user/IntArea/dto/image/TestImageDto.java
+++ src/main/java/com/user/IntArea/dto/image/TestImageDto.java
@@ -3,9 +3,12 @@ package com.user.IntArea.dto.image;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.UUID;
+
 @Data
 public class TestImageDto {
 
+    private UUID refId;
     private String title;
     private MultipartFile image;
 }
