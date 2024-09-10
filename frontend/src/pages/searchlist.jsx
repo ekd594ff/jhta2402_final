7893a18c5f6289a38491b723a8fcbd6b141b2742
SangBin0902 <SangBin0902@gmail.com>
2024-09-10 15:32:32 +0900

    commit for switch

--- frontend/src/pages/searchlist.jsx
+++ frontend/src/pages/searchlist.jsx
@@ -1,130 +1,82 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import {
-  Typography,
-  List,
-  ListItem,
-  ListItemText,
-  CircularProgress,
-  Button,
-  Grid,
-} from "@mui/material";
-import { useLocation, useNavigate } from "react-router-dom";
-import Header from "../components/common/header";
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {Chip, Divider} from '@mui/material';
+import {useLocation} from 'react-router-dom';
+
+import Header from '../components/common/header';
+import Footer from "../components/common/footer.jsx";
+import SearchListItem from "../components/searchlist/search-list-item.jsx";
+import style from "../styles/serachlist.module.scss";
 
 function SearchList() {
   const location = useLocation();
-  const navigate = useNavigate();
-  const query = new URLSearchParams(location.search).get("query");
+  const query = new URLSearchParams(location.search).get('query');
+  const [prevQuery, setPrevQuery] = useState("");
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [page, setPage] = useState(0);
   const [hasMore, setHasMore] = useState(true);
 
+  const fetchResults = (pageToFetch = 0, reset = false) => {
+    setLoading(true);
+    setError(null);
+    axios.get(`/api/portfolio/search/detailed?searchWord=${query}&page=${pageToFetch}&size=4`)
+        .then(result => {
+          setResults(prevResults => reset ? result.data.content : [...prevResults, ...result.data.content]);
+          setHasMore(result.data.page.totalElements > (reset ? result.data.content.length : results.length + result.data.content.length));
+        })
+        .catch(() => setError('검색 결과를 가져오는 데 실패했습니다.'))
+        .finally(() => {
+          setLoading(false);
+          setPrevQuery(query);
+        });
+  };
+
   useEffect(() => {
-    const fetchSearchResults = async () => {
-      setLoading(true);
-      setError(null);
-
-      try {
-        const response = await axios.get(
-          `/api/portfolio/search/detailed?searchWord=${query}&page=${page}&size=10`
-        );
-        setResults((prevResults) => [...prevResults, ...response.data.content]);
-        setHasMore(response.data.content.length > 0); // 다음 페이지가 있는지 확인
-      } catch (err) {
-        setError("검색 결과를 가져오는 데 실패했습니다.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    if (query) {
-      fetchSearchResults();
+    if (prevQuery !== query) {
+      setPage(0);
+      fetchResults(0, true);
     }
-  }, [query, page]);
+  }, [query, prevQuery]);
+
+  useEffect(() => {
+    if (page > 0) {
+      fetchResults(page);
+    }
+  }, [page]);
 
   const loadMoreResults = () => {
-    if (hasMore) {
-      setPage((prevPage) => prevPage + 1);
+    if (hasMore && !loading) {
+      setPage(prevPage => prevPage + 1);
     }
   };
 
-  const handleItemClick = (id) => {
-    navigate(`/portfolio/${id}`);
-  };
-
-  if (loading && page === 0) {
-    return <CircularProgress />;
-  }
-  if (error) {
-    return <Typography color="error">{error}</Typography>;
-  }
-
   return (
-    <div>
-      <Header />
-      <Typography
-        variant="h6"
-        style={{ marginTop: "80px", marginLeft: "30px" }}
-      >
-        " {query} "의 검색 결과
-      </Typography>
-      <Grid>
-        {results.length > 0 ? (
-          results.map((item, index) => (
-            <ListItem
-              key={index}
-              divider
-              onClick={() => {
-                console.log("portfolioId: ", item.id);
-                handleItemClick(item.id);
-              }}
-            >
-              <ListItemText
-                primary={item.title}
-                secondary={
-                  <>
-                    <span style={{ color: "black" }}>{item.description}</span>
-                    <br />
-                    <span style={{ color: "gray" }}>{item.companyName}</span>
-                  </>
-                }
-              />
-              <Grid container spacing={1}>
-                {item.imageUrls &&
-                  item.imageUrls.map((url, imgIndex) => (
-                    <Grid item key={imgIndex} style={{ marginLeft: "8px" }}>
-                      <img
-                        src={`https://picsum.photos/seed/${imgIndex}/600/400`}
-                        alt={`image-${imgIndex}`}
-                        style={{
-                          width: "100px",
-                          height: "auto",
-                          borderRadius: "4px",
-                        }}
-                      />
-                    </Grid>
-                  ))}
-              </Grid>
-            </ListItem>
-          ))
-        ) : (
-          <Typography>No results found</Typography>
-        )}
-      </Grid>
-      {hasMore && (
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={loadMoreResults}
-          disabled={loading}
-        >
-          {loading ? "Loading..." : "더보기"}
-        </Button>
-      )}
-    </div>
+      <>
+        <Header/>
+        <main className={style['searchList']}>
+          <div className={style['container']}>
+            <div className={style['search-title']}><span>{`'${query}'`}</span>{`에 대한 검색결과`}</div>
+            {!loading && !results.length ?
+                <div className={style['empty']}>
+                  <span>검색 결과가 존재하지 않습니다</span>
+                </div> :
+                <>
+                  <ul className={style['list']}>
+                    {results.map((item, index) => <SearchListItem key={index} {...item}/>)}
+                  </ul>
+                  {hasMore && !loading && (
+                      <Divider>
+                        <Chip className={style['more']} label="더보기" size="small" variant="outlined"
+                              onClick={loadMoreResults}/>
+                      </Divider>
+                  )}
+                </>}
+          </div>
+        </main>
+        <Footer/>
+      </>
   );
 }
 
