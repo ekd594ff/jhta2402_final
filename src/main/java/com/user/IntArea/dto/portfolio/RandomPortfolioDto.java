2134fc891a823a618913a33227af567271596193
ekd594ff <ekd594ff@gmail.com>
2024-09-12 17:11:51 +0900

    fix: fix random portfolio api

--- src/main/java/com/user/IntArea/dto/portfolio/RandomPortfolioDto.java
+++ src/main/java/com/user/IntArea/dto/portfolio/RandomPortfolioDto.java
@@ -1,4 +1,15 @@
 package com.user.IntArea.dto.portfolio;
 
-public class RandomPortfolioDto {
+import com.user.IntArea.entity.Portfolio;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class RandomPortfolioDto extends PortfolioInfoDto {
+    private String thumbnail;
+
+    public RandomPortfolioDto(UUID id, String title, String description, String companyName, LocalDateTime createdAt, LocalDateTime updatedAt, boolean isDeleted, String url) {
+        super(id, title, description, companyName, createdAt, updatedAt, isDeleted);
+        this.thumbnail = url;
+    }
 }
