9d90299525616614e337ff4e9430606316a7b7fb
jin0815b <wlsgus25843@gmail.com>
2024-09-19 12:12:01 +0900

    fix : search-sort

--- src/main/java/com/user/IntArea/repository/CustomPortfolioRepository.java
+++ src/main/java/com/user/IntArea/repository/CustomPortfolioRepository.java
@@ -1,9 +1,14 @@
 package com.user.IntArea.repository;
 
+import com.user.IntArea.entity.Portfolio;
 import jakarta.persistence.Tuple;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface CustomPortfolioRepository {
-    Page<Tuple> findPortfolioBySearchWord(String searchWord,String sortField, String sortDirection, Pageable pageable);
+    Page<Tuple> findPortfolioBySearchWord(String searchWord, String sortField, String sortDirection, Pageable pageable);
+
+//    List<Tuple> getRecommendedPortfolioByAvgRate();
 }
